13b1e25fe308ee2d5303e767bdd2302be4ee1f0a
JackBenny39 <charlesdcollver@gmail.com>
2017-11-12 11:59:58 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='pyziabm',
+   version='v0.0.2b',
+   description='ZI ABM',
+   author='Jack Benny',
+   author_email='jack@jackbenny.com',
+   packages=['pyziabm']  #same as name
+)
