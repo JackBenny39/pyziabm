c65dc479a4963ac9f139ffb61e4cc0f94d892843
JackBenny39 <charlesdcollver@gmail.com>
2017-11-12 12:07:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='pyziabm',
-   version='v0.0.2b',
+   version='v0.0.2c',
    description='ZI ABM',
    author='Jack Benny',
    author_email='jack@jackbenny.com',
